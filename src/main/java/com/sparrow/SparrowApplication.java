af16420060542d9bcb08771dc341906802736f79
Pavle Jankovic <pavle.gp@gmail.com>
2018-10-26 13:57:53 +0200

    Initial commit, basic dependencies and configuration

---
+++ src/main/java/com/sparrow/SparrowApplication.java
@@ -0,0 +1,12 @@
+package com.sparrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparrowApplication.class, args);
+    }
+}
