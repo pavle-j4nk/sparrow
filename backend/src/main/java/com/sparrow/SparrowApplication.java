0d15c5f1fb91a66d5ad46f057b7599dccd82240b
Pavle Jankovic <pavle.gp@gmail.com>
2018-12-15 16:47:20 +0100

    Slack commit test

--- backend/src/main/java/com/sparrow/SparrowApplication.java
+++ backend/src/main/java/com/sparrow/SparrowApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SparrowApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SparrowApplication.class, args);
     }
