d1ab7c70df1023142c16bf14fe9b17f856f438fc
Pavle Jankovic <pavle.gp@gmail.com>
2018-12-25 16:23:42 +0100

    Pretraga po imenu, dodatni filteri

--- backend/src/main/java/com/sparrow/service/impl/user/UserServiceImpl.java
+++ backend/src/main/java/com/sparrow/service/impl/user/UserServiceImpl.java
@@ -1,14 +1,16 @@
 package com.sparrow.service.impl.user;
 
+import com.sparrow.model.user.FriendRequest;
 import com.sparrow.model.user.User;
-import com.sparrow.repository.user.FriendshipRepository;
 import com.sparrow.repository.user.UserRepository;
 import com.sparrow.response.UserProfileResponse;
 import com.sparrow.response.UserResponse;
+import com.sparrow.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -20,7 +22,7 @@ public class UserServiceImpl implements UserService {
     UserRepository userRepository;
 
     @Autowired
-    FriendshipRepository friendshipRepository;
+    FriendshipService friendshipService;
 
     @Override
 
@@ -63,7 +65,40 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> searchByAnyName(String name) {
-        return userRepository.findAllWhereAnyNameContains(name);
+        return userRepository.findAllWhereAnyNameContains(name.toLowerCase());
+    }
+
+    @Override
+    public List<User> searchByAnyName(String name, String invoker, Boolean isFriend, Boolean canAddFriend) {
+        List<User> users = searchByAnyName(name);
+
+        if (users.isEmpty() || (isFriend == null && canAddFriend == null)) {
+            return users;
+        }
+
+        List<User> friends = friendshipService.getFriendsOf(invoker);
+
+        if (isFriend != null) {
+            if (isFriend) {
+                users.removeIf(u -> !friends.contains(u));
+            } else {
+                users.removeIf(u -> friends.contains(u));
+            }
+        }
+
+        if (canAddFriend != null) {
+            List<FriendRequest> requests = friendshipService.getRequestOf(invoker);
+            List<User> requestsTo = new ArrayList<>();
+            requests.stream().forEach(r -> requestsTo.add(r.getReceiver()));
+
+            if (canAddFriend) {
+                users.removeIf(u -> friends.contains(u) || requestsTo.contains(u));
+            } else {
+                users.removeIf(u -> !(friends.contains(u) || requestsTo.contains(u)));
+            }
+        }
+
+        return users;
     }
 
 
