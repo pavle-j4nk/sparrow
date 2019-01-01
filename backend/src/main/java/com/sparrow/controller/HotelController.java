67752b2f1710325b9d0d47d12bebc74ca42f2b81
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-01-01 19:52:08 +0100

    Added logger messages in HotelController

--- backend/src/main/java/com/sparrow/controller/HotelController.java
+++ backend/src/main/java/com/sparrow/controller/HotelController.java
@@ -2,6 +2,8 @@ package com.sparrow.controller;
 
 import com.sparrow.model.hotel.Hotel;
 import com.sparrow.service.HotelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,26 +18,28 @@ import java.util.List;
 @RequestMapping(value = "api/public/hotels")
 public class HotelController {
 
+    private Logger logger = LoggerFactory.getLogger(HotelController.class);
+
     @Autowired
     HotelService hotelService;
 
     @GetMapping
     public ResponseEntity<List<Hotel>> getHotels() {
-            return ResponseEntity.ok(hotelService.findAll());
+        return ResponseEntity.ok(hotelService.findAll());
     }
 
     @PostMapping
     public ResponseEntity<Hotel> postHotel(@RequestBody Hotel hotel) {
-        System.err.println("In postHotel...");
+        logger.info("Creating new hotel \n");
+        logger.info(String.format("Name: %s \nDescription: %s", hotel.getName(), hotel.getDescription()));
+        logger.info("Saving hotel to database...");
         return ResponseEntity.ok(hotelService.save(hotel));
     }
 
 
     @GetMapping(value = "/details/**")
     public String getHotelDetails() {
-        System.err.println("Hotel details..");
+        logger.info("Getting hotel details...");
         return "/index.html";
     }
-
-
 }
