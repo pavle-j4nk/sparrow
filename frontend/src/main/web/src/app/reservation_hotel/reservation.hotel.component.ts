40b7605b70901138fea16973fbca1589fd9783c8
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-07-16 10:06:28 +0200

    Architecture fix

--- frontend/src/main/web/src/app/reservation_hotel/reservation.hotel.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector : 'reservation-hotel',
-  templateUrl : './reservation.hotel.component.html'
-})
-export class HotelReservationComponent {
-
-}
--- sparrow/src/main/java/com/sparrow/repository/user/PrivilegeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparrow.repository.user;
-
-import com.sparrow.model.Privilege;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
-    Privilege findByName(String name);
-}
