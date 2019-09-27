20e44dd465aba97b00d7bf5faec43057f4a030c9
Markaya <markoristic15@gmail.com>
2019-09-27 19:11:06 +0200

    lock na carRepo

--- src/main/java/com/sparrow/backend/repository/CarRepository.java
+++ src/main/java/com/sparrow/backend/repository/CarRepository.java
@@ -2,9 +2,13 @@ package com.sparrow.backend.repository;
 
 import com.sparrow.backend.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
 
 public interface CarRepository extends JpaRepository<Car , Long> {
 
-
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    Car save(Car car);
 
 }
--- src/main/java/com/sparrow/backend/service/CarService.java
+++ src/main/java/com/sparrow/backend/service/CarService.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.Lock;
 import javax.persistence.LockModeType;
 
 public interface CarService {
-    @Lock(LockModeType.PESSIMISTIC_READ)
+
     Car createCar(Car car , Long rentaCarId);
 
     Car findById(Long id);
 
     void delete(Car car);
 
-    @Lock(LockModeType.PESSIMISTIC_READ)
+
     Car updateCar(Car newCar, Long oldCar);
 }
